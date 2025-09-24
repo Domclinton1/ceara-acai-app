b9bbac764c7c9846132adafe1792fc075ccb5661
Domclinton1 <clinton.admpro29@gmail.com>
2025-09-24 11:52:25 -0300

    ...

--- ceara-app/src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ ceara-app/src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format-currency";
 
 interface ProductDetailsProps {
@@ -35,8 +36,8 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
     setQuantity((prev) => prev + 1);
   };
   return (
-    <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl p-5 flex flex-auto flex-col">
-      <div className="flex-auto">
+    <div className="relative z-50 mt-[-1.5rem] rounded-tl-3xl p-5 flex flex-auto flex-col overflow-hidden">
+      <div className="">
         {/*RESTAURANTE*/}
         <div className="flex items-center gap-1.5">
           <Image
@@ -77,20 +78,26 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
           </div>
         </div>
 
-        {/*SOBRE*/}
-        <div className="mt-6 space-y-3">
-          <h4 className="font-semibold">Sobre</h4>
-          <p className="text-sm text-muted-foreground">{product.description}</p>
-        </div>
-
-        {/*SOBRE*/}
-        <div className="mt-6 space-y-3">
-          <div className="flex items-center gap-1">
-            <ChefHatIcon size={18} />
-            <h4 className="font-semibold">Ingredientes</h4>
+        <ScrollArea className="h-full">
+          {/*SOBRE*/}
+          <div className="mt-6 space-y-3">
+            <h4 className="font-semibold">Sobre</h4>
+            <p className="text-sm text-muted-foreground">
+              {product.description}
+            </p>
           </div>
-          <p className="text-sm text-muted-foreground">{product.ingredients}</p>
-        </div>
+
+          {/*INGREDIENTES*/}
+          <div className="mt-6 space-y-3">
+            <div className="flex items-center gap-1">
+              <ChefHatIcon size={18} />
+              <h4 className="font-semibold">Ingredientes</h4>
+            </div>
+            <p className="text-sm text-muted-foreground">
+              {product.ingredients}
+            </p>
+          </div>
+        </ScrollArea>
       </div>
       <Button className="rounded-full w-full mt-6">Adcionar à sacola</Button>
     </div>
